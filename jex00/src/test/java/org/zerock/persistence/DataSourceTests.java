354ab48872f6e51113e31a591b77768c24405fd1
Standard-B <wjdtjr030@naver.com>
2023-08-28 19:59:00 +0900

    hikariCP connection 테스트코드 작성

--- jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -11,14 +11,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 //JAVA 설정을 사용하는 경우
-//@ContextConfiguration(classes = {RootConfig.class})
+@ContextConfiguration(classes = {RootConfig.class})
 @Log4j
 public class DataSourceTests {
 	
